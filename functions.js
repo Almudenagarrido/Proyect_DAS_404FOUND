f2950f1598455cdb2a632e8209ead5511ed08bbc
Lucia <202214497@alu.comillas.edu>
2025-02-05 17:52:09 +0100

    js terminado!

--- functions.js
+++ functions.js
@@ -4,12 +4,8 @@ form.addEventListener("submit", function (event) {
     event.preventDefault();
 
     let camposValidos = comprobarCampos();
-    let correoValido = validarCorreo();
-    let dniValido = validarDNI();
-    //let contraseniaValida = validarContrasenia();
-    let nombreApellidoValido = validarNombreApellido();
 
-    if (camposValidos && correoValido && dniValido && contraseniaValida && validarNombreApellido) {
+    if (camposValidos) {
         form.submit();
     }
 });
@@ -41,112 +37,92 @@ function comprobarCampos() {
     return valid;
 }
 
-function validarCorreo() {
-    let valid = true;
-    const mailInput = document.getElementById("mail");
-
-    const mailPattern = /^[a-zA-Z0-9._%+-]+@comillas\.edu$/;
-
-    if (!mailPattern.test(mailInput.value.trim())) {
-        valid = false;
-        mailInput.placeholder = "usuario@comillas.edu";
-        mailInput.style.border = "2px solid red";
-    } 
-    else {
-        mailInput.style.border = "";
-    }
-
-    mailInput.addEventListener("input", function () {
-        mailInput.style.border = "";
-    });
-
-    return valid;
-}
-
-function validarDNI() {
-
-    let valid = true;
-    const dniInput = document.getElementById("dni");
-    const dniPattern = /^([0-9]{8})([A-Z]{1})$/;
-
-    if (!dniPattern.test(dniInput.value.trim())) {
-        valid = false;
-        dniInput.placeholder = "54327401B";
-        dniInput.style.border = "2px solid red";
-    }
-    else {
-        dniInput.style.border = "";
-    }
-
-    dniInput.addEventListener("input", function () {
-        mailInput.style.border = "";
-    });
-
-    return valid;
-}
-
-function validarNombreApellido() {
-
-    let valid = true;
-    const name = document.getElementById("name");
-    const surname = document.getElementById("surname");
+document.getElementById("name").addEventListener("blur", function(){
     const pattern = /^[a-zA-Z]+$/;
 
-    if (!pattern.test(name.value.trim())) {
-        valid = false;
-        name.style.border = "2px solid red";
+    if (!pattern.test(this.value.trim())) {
+        this.style.border = "2px solid red";
     }
-
-    if (!pattern.test(surname.value.trim())) {
-        valid = false;
-        name.style.border = "2px solid red";
+    else {
+        this.style.border = "";
     }
+})
 
-    name.addEventListener("input", function () {
-        name.style.border = "";
-    });
+document.getElementById("surname").addEventListener("blur", function(){
+    const pattern = /^[a-zA-Z]+$/;
 
-    surname.addEventListener("input", function () {
-        surname.style.border = "";
-    });
+    if (!pattern.test(this.value.trim())) {
+        this.style.border = "2px solid red";
+    }
+    else {
+        this.style.border = "";
+    }
+})
 
-    return valid;
-}
 
-// function validarContrasenia()
-// {
-//     let valid = true;
-//     const psw = document.getElementById("psw");
-//     const confirmationpsw = document.getElementById("confirmationpsw");
+document.getElementById("dni").addEventListener("keyup", function(){
+    const dniPattern = /^([0-9]{8})([A-Z]{1})$/;
 
-//     if (psw.value != confirmationpsw.value)
-//     {
-//         valid = false;
-//         psw.style.border = "2px solid red";
-//         confirmationpsw.style.border = "2px solid red";
-//     }
-//     else {
-//         psw.style.border = "";
-//         confirmationpsw.style.color = "";
-//     }
+    if (!dniPattern.test(this.value.trim())) {
+        this.placeholder = "54327401B";
+        this.style.border = "2px solid red";
+    }
+    else {
+        this.style.border = "";
+    }
+})
 
-//     psw.addEventListener("input", function () {
-//         psw.style.border = "";
-//     });
+document.getElementById("mail").addEventListener("blur", function (){
+    const mailInput = this.value
+    const mailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const spanMail = document.getElementById("spanMail");
+
+    if (!mailPattern.test(mailInput.trim())){
+        this.style.border = "2px solid red";
+        spanMail.style.display = "block";
+    } 
+    else{
+        this.style.border = "";
+        spanMail.style.display = "none";
+    }
+})
+
+
+document.getElementById("psw").addEventListener("keyup", function () {
+    const spanpswl = document.getElementById("spanpswl");
     
-//     confirmationpsw.addEventListener("input", function () {
-//         confirmationpsw.style.border = "";
-//     });
+    if (this.value.length < 8){
+        this.style.border = "2px solid red";
+        spanpswl.style.display = "block";
+    }
+    else {
+        spanpswl.style.display = "none";
+        this.style.border = "";
+    }
+})
+
+
+document.getElementById("psw").addEventListener("blur", function () {
+    const spanpswl = document.getElementById("spanpswl");
     
-//     return valid;
-// }
+    if (this.value.length < 8){
+        this.classList.add("shake");
+
+        setTimeout(() => {
+            this.classList.remove("shake");
+        }, 200);
+    }
+    else {
+        spanpswl.style.display = "none";
+    }
+})
 
 document.getElementById("confirmationpsw").addEventListener("keyup", function () {
     
     const psw = document.getElementById("psw");
     const spanpsw = document.getElementById("spanpsw");
 
-    if (this.value !== psw.value)
+    if (this.value !== psw.value) 
     {
         this.style.border = "2px solid red";
         psw.style.border = "2px solid red";
